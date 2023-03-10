75beaf3ea7f93d2b1845edf06010c4f88f4fb04b
diptiequals <diptiequals@gmail.com>
2023-03-10 07:30:18 +0530

    10th March 7.30 am

--- Dummy9March/src/test/java/package1/Sample1.java
+++ Dummy9March/src/test/java/package1/Sample1.java
@@ -4,7 +4,7 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Lets Live");
+		System.out.println("Hi Dipti");
 		
 
 	}
